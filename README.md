a6f436b67cdad98ecd69d664f6c94325e6281b37
John Slater <slater.john@gmail.com>
2021-12-24 11:59:27 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # lighthouse-a11y-audit
 Run Lighthouse accessibility reports against multiple websites and see an overview of the results.
+
+Disclaimer: this is hacked together to serve a purpose. It doesn't handle errors well and it's not that customisable. It just runs a11y lighthouse tests against URLs you give it.
+
+## Installation
+Requires Node v16.13.1
+
+Clone this repo and run.
+
+	npm install
+
+## Settings
+Make changes to the included the `settings.json` file to run the audits against your chosen URLs.
+
+	{
+		"reportsLocation": "./reports",
+		"sites": [
+			{
+				"title": "Trafford Housing Trust",
+				"folder": "traffordhousingtrust.co.uk",
+				"urls": [
+					{
+						"title": "Homepage",
+						"url": "https://www.traffordhousingtrust.co.uk"
+					},
+					{
+						"title": "Content page - Active appetites",
+						"url": "https://www.traffordhousingtrust.co.uk/advice-and-support/active-appetites/"
+					}
+				]
+			}
+		]
+	}
+
+| Option      | Description |
+| ----------- | ----------- |
+| reportsLocation      | Sets the output location of reports and summaries.       |
+
+### Sites
+| Option      | Description |
+| ----------- | ----------- |
+| title      | Title of the website or project. Used in report titles.       |
+| folder      | Name of the folder for reports to be stored in. Must be unique.       |
+| urls      | Array of URLs within your website or project.       |
+
+### URLs
+| Option      | Description |
+| ----------- | ----------- |
+| title      | Title of the page. Used in the reports.       |
+| url      | URL of the page being tested.       |
+
+## Run
+The script will test each URL sequentially and generate a report for each site/project with a breakdown of a11y issues on each page.
+
+	node index.js
---
+++ index.js
@@ -0,0 +1,205 @@
+const fs = require('fs');
+const lighthouse = require('lighthouse');
+const chromeLauncher = require('chrome-launcher');
+const logger = require('node-color-log');
+const Handlebars = require("handlebars");
+const moment = require("moment");
+const settings = require("./settings.json");
+
+var results = [];
+
+async function runTest(opt, site){
+	const location = opt.location + '/' + encodeURI(opt.title);
+	const locationJson = location + '.json';
+	const locationHTML = location + '.html';
+
+	const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+	const options = {
+		// logLevel: 'info',
+		logLevel: false,
+		output: ['json', 'html'],
+		onlyCategories: ['accessibility'],
+		port: chrome.port
+	};
+
+	logger.info('Starting: ' + opt.url);
+
+	const runnerResult = await lighthouse(opt.url, options);
+
+	// HTML
+	const reportHTML = runnerResult.report[1];
+	fs.writeFileSync(locationHTML, reportHTML);
+
+	// JSON
+	const reportJsonText = runnerResult.report[0];
+	const reportJson = JSON.parse(reportJsonText);
+	fs.writeFileSync(locationJson, reportJsonText);
+
+	// Setup results
+	var result = {
+		"title": opt.title,
+		"url": opt.url,
+		"jsonReportURL": locationJson,
+		"htmlReportURL": locationHTML,
+		"score": 0,
+		"scorePercent": 0,
+		"bsScoreClass": '',
+		"failures": [],
+		"successes": [],
+		"other": [],
+	};
+
+	// Scores = 0 are fails, null = na, 1 = pass
+	// Find all the fails in audits
+	for(var i in reportJson.audits){
+		var key = i;
+		var value = reportJson.audits[i];
+
+		if(value.score == 0) result['failures'].push(value);
+		else if(value.score == 1) result['successes'].push(value);
+		else if(value.score == null) result['other'].push(value);
+	}
+
+	// Set the overall score
+	result.score = runnerResult.lhr.categories.accessibility.score;
+	result.scorePercent = runnerResult.lhr.categories.accessibility.score * 100;
+
+	// Set score classes
+	result.bsScoreClass = classForScore(result.score);
+
+	logger.debug('Score: ' + result.score);
+	logger.info('Finished: ' + opt.url);
+	await chrome.kill();
+
+	return result;
+
+};
+
+
+async function runSite(site){
+	var dir = settings.reportsLocation;
+
+	// Setup results object
+	var thisSiteResult = {...site, ...{"results": []}};
+
+	thisSiteResult['dateTime'] = moment().format("");
+	thisSiteResult['dateTimeFriendly'] = moment().format("Do MMM YYYY [at] HH:mm");
+	thisSiteResult['dateFriendly'] = moment().format("Do MMM YYYY");
+	thisSiteResult['timeFriendly'] = moment().format("HH:mm");
+
+	// Create reports folder
+	if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+	// Create site report folder
+	var siteDir = dir + '/' + site.folder;
+	if (!fs.existsSync(siteDir)) fs.mkdirSync(siteDir);
+
+	// Run test on each url
+	for (const url of site.urls){
+		url.location = siteDir;
+		var result = await runTest(url, site);
+		thisSiteResult.results.push(result);
+	};
+
+	results.push(thisSiteResult);
+	makeSummaryPage(site, thisSiteResult);
+
+}
+
+async function runAllSites(){
+	// Loop over each site
+	for (const site of settings.sites){
+		await runSite(site);
+	}
+
+	makeReportsPage();
+}
+
+function makeSummaryPage(site, thisSiteResult){
+	// Get template summary file
+	var source;
+	try {
+		source = fs.readFileSync('./templates/summary.html', 'utf8');
+	} catch (err) {
+		logger.error(err);
+	}
+
+	// Handlebars the file
+	var template = Handlebars.compile(source);
+	var summaryResult = template(thisSiteResult);
+
+	// Save summary
+	const nowTimeDate = moment().format("");
+	const summaryLoc = settings.reportsLocation + '/' + site.folder + '.html';
+	fs.writeFileSync(summaryLoc, summaryResult);
+}
+
+function makeReportsPage(sites){
+
+	// Manipulate data object
+	gatherResultsData();
+
+	// Get template file
+	var source;
+	try {
+		source = fs.readFileSync('./templates/reports.html', 'utf8');
+	} catch (err) {
+		logger.error(err);
+	}
+
+	// Handlebars the file
+	var template = Handlebars.compile(source);
+	var summaryResult = template({results: results});
+
+	// Save summary
+	const nowTimeDate = moment().format("");
+	const summaryLoc = settings.reportsLocation + '/' + 'index.html';
+	fs.writeFileSync(summaryLoc, summaryResult);
+
+	// Launch report
+	chromeLauncher.launch({
+		startingUrl: settings.reportsLocation + '/' + 'index.html'
+	}).then(chrome => {
+		logger.info('Opening report');
+	});
+}
+
+function gatherResultsData(){
+
+	// Loop each site
+	results.forEach((site, i) => {
+		var siteTotalScore = 0;
+		var siteTotalFails = 0;
+		var siteTotalSuccesses = 0;
+
+		// Loop each site result
+		site.results.forEach((result, i) => {
+			siteTotalScore += result.score;
+			siteTotalFails += result.failures.length;
+			siteTotalSuccesses += result.successes.length;
+		});
+
+		// Average Score
+		site.averageScore = siteTotalScore / site.results.length;
+		site.averageScorePercent = Math.round(site.averageScore * 100);
+		site.bsScoreClass = classForScore(site.averageScore);
+
+		// Totals
+		site.totalFails = siteTotalFails;
+		site.totalSuccesses = siteTotalSuccesses;
+		site.totalChecks = site.totalFails + site.totalSuccesses;
+
+	});
+
+}
+
+function classForScore(score){
+	if(score >= .9) return 'success';
+	else if(score >= .5) return 'warning dark-text';
+	else if(score < .5) return 'danger';
+	else return 'light dark-text';
+}
+
+runAllSites();
+// Run specific site
+// runSite(settings.sites[0]);
